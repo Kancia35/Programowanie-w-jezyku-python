24e33266d3938ab353c27c1dcb86ad0e688439d4
Karolina <skorulska.k@gmail.com>
2025-12-16 22:49:40 +0100

    add screen

---
+++ Lista imion.py
@@ -0,0 +1,8 @@
+def wyswietl_imiona(lista_imion):
+
+  print("Wyświetlanie imion:")
+  for imie in lista_imion:
+    print(imie)
+imiona = ["Karolina", "Alicja", "Michał", "Dominik", "Weronika"]
+wyswietl_imiona(imiona)
+
