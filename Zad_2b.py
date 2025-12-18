708c3fa9b98e4c426e6250da208dc07ac6fd1dbe
Karolina <skorulska.k@gmail.com>
2025-12-18 22:21:00 +0100

    zadania

--- Zad_2b.py
+++ Zad_2b.py
@@ -9,3 +9,10 @@ liczby = [2, 4, 6, 8, 9]
 print(mnozenie(liczby))
 
 
+
+def pomnoz_przez_dwa(liczba:list):
+    return [x * 2 for x in lista]
+
+liczby = [2, 4, 6, 8, 9]
+wynik = pomnoz_przez_dwa(liczby)
+print(wynik)
