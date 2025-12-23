075be68fca4b744e3dcd7bfa246cba57587d970b
Karolina <skorulska.k@gmail.com>
2025-12-24 00:19:11 +0100

    formating

--- Zad_2a.py
+++ Zad_2a.py
@@ -1,10 +1,9 @@
-def names(name : list):
+def names(name: list):
+    print('Wyświetlanie imion:')
+    for x in name:
+        print(x)
 
-  print('Wyświetlanie imion:')
-  for x in name:
-    print(x)
 
 if __name__ == '__main__':
     example = ['Karolina', 'Alicja', 'Michał', 'Dominik', 'Weronika']
     names(example)
-
--- Zad_2b.py
+++ Zad_2b.py
@@ -1,21 +1,19 @@
-def multiple_v1(number:list) -> list:
+def multiple_v1(number: list) -> list:
     extralist = []
-    for x  in number:
-        newnumber=x*2
+    for x in number:
+        newnumber = x * 2
         extralist.append(newnumber)
     return extralist
 
 
-def multiple_v2(number:list) -> list:
+def multiple_v2(number: list) -> list:
     return [x * 2 for x in number]
 
 
-
-
 if __name__ == '__main__':
-#version 1
+    # version 1
     example_v1 = [2, 4, 6, 8, 9]
     print(multiple_v1(example_v1))
-#version 2
+    # version 2
     example_v2 = [2, 4, 6, 8, 10]
     print(multiple_v2(example_v2))
